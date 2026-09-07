1b66daae856f8bb37c30e73dec593a618d0498f9
Igor Chernakov <igor.chernakov@gmail.com>
2026-09-07 20:53:55 +0200

    wow: link loading callback in game tests

--- games/world-of-warcraft/tests/test_wow_abilities.c
+++ games/world-of-warcraft/tests/test_wow_abilities.c
@@ -151,6 +151,7 @@ static HANDLE test_read_file(LPCSTR filename, LPDWORD size) {
 void UI_WriteWowHud(LPEDICT ent) { (void)ent; }
 void UI_WriteWowHover(LPEDICT ent) { (void)ent; }
 void UI_WriteWelcomeWindow(LPEDICT ent) { (void)ent; }
+void UI_WriteLoadingLayout(LPEDICT ent) { (void)ent; }
 void UI_HideWindow(LPEDICT ent, LPCSTR window_id) { (void)ent; (void)window_id; }
 
 static struct game_import test_import(void) {
