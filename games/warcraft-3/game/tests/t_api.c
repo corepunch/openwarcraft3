50eae309eeb93b044371f6b3f700c56941289e8e
sookyboo <sookyboo@gmail.com>
2026-09-10 09:30:50 +0100

    fix(wc3): harden transmission message log handling

--- games/warcraft-3/game/tests/t_api.c
+++ games/warcraft-3/game/tests/t_api.c
@@ -1229,6 +1229,13 @@ TEST(wc3_api, narrator_and_hint_text_share_message_log) {
     T_EQ(gc->message_log.count, 2);
     T_STREQ(gc->message_log.entries[0], "|cffffcc00Narrator:|r Select a peon.");
     T_STREQ(gc->message_log.entries[1], "HINT - Build a Burrow.");
+
+    memset(&gc->message_log, 0, sizeof(gc->message_log));
+    T_ASSERT(run_test_jass(
+        "function main takes nothing returns nothing\n"
+        "  call SetCinematicScene(0, PLAYER_COLOR_RED, \"\", \"\", 0.0, 0.0)\n"
+        "endfunction\n"));
+    T_EQ(gc->message_log.count, 0);
 }
 
 TEST(wc3_api, transient_command_style_text_does_not_enter_message_log) {
